f4d1286994c6fd992413cdb1b0be3fe8e61bae2f
Andrew Block <andy.block@gmail.com>
2016-06-21 16:38:26 -0400

    Git hosting on gogs and migrated jobs to use gogs server and jenkinsfiles

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 SCRIPT_BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
@@ -15,6 +15,13 @@ OSE_CI_PROJECT="ci"
 OSE_SWARM_APP_DEV="swarm-dev"
 OSE_SWARM_APP_QA="swarm-qa"
 OSE_SWARM_APP_PROD="swarm-prod"
+
+
+POSTGRESQL_USER="postgresql"
+POSTGRESQL_PASSWORD="password"
+POSTGRESQL_DATABASE="gogs"
+GOGS_ADMIN_USER="gogs"
+GOGS_ADMIN_PASSWORD="osegogs"
 
 
 function wait_for_running_build() {
@@ -37,6 +44,27 @@ function wait_for_running_build() {
 
     set -e
 
+}
+
+function wait_for_endpoint_registration() {
+    ENDPOINT=$1
+    NAMESPACE=$2
+    
+    set +e
+    
+    while true
+    do
+        oc get ep $ENDPOINT -n $NAMESPACE -o yaml | grep "\- addresses:"
+        
+        if [ $? -eq 0 ]; then
+            break
+        fi
+        
+        sleep 10
+        
+    done
+
+    set -e
 }
 
 # Login to CDK
@@ -126,6 +154,79 @@ wait_for_running_build "nexus" "${OSE_CI_PROJECT}" "2"
 oc build-logs -n ${OSE_CI_PROJECT} -f nexus-2
 
 
+echo
+echo "Deploying PostgreSQL for Gogs..."
+echo
+oc process -f $SCRIPT_BASE_DIR/support/templates/postgresql-ephemeral.json -v=POSTGRESQL_DATABASE=$POSTGRESQL_DATABASE,POSTGRESQL_USER=$POSTGRESQL_USER,POSTGRESQL_PASSWORD=$POSTGRESQL_PASSWORD  | oc create -n $OSE_CI_PROJECT -f-
+
+wait_for_endpoint_registration "postgresql" "$OSE_CI_PROJECT"
+
+echo
+echo "Deploying Gogs Server..."
+echo
+oc process -f $SCRIPT_BASE_DIR/support/templates/gogs-template.json | oc create -n $OSE_CI_PROJECT -f-
+
+wait_for_endpoint_registration "gogs" "$OSE_CI_PROJECT"
+
+# Determine Running Pod
+GOGS_POD=$(oc get pods -n $OSE_CI_PROJECT -l=deploymentconfig=gogs --no-headers | awk '{ print $1 }')
+
+GOGS_ROUTE=$(oc get routes -n $OSE_CI_PROJECT gogs --template='{{ .spec.host }}')
+
+# Sleep before setting up gogs server
+sleep 5
+
+# Setup Server
+HTTP_RESPONSE=$(curl -o /dev/null -sL -w "%{http_code}" http://$GOGS_ROUTE/install \
+--form db_type=PostgreSQL \
+--form db_host=postgresql:5432 \
+--form db_user=$POSTGRESQL_USER \
+--form db_passwd=$POSTGRESQL_PASSWORD \
+--form db_name=$POSTGRESQL_DATABASE \
+--form ssl_mode=disable \
+--form db_path=data/gogs.db \
+--form "app_name=Gogs: Go Git Service" \
+--form repo_root_path=/home/gogs/gogs-repositories \
+--form run_user=gogs \
+--form domain=localhost \
+--form ssh_port=22 \
+--form http_port=3000 \
+--form app_url=http://$GOGS_ROUTE/ \
+--form log_root_path=/opt/gogs/log \
+--form admin_name=$GOGS_ADMIN_USER \
+--form admin_passwd=$GOGS_ADMIN_PASSWORD \
+--form admin_confirm_passwd=$GOGS_ADMIN_PASSWORD \
+--form admin_email=gogs@redhat.com)
+
+if [ $HTTP_RESPONSE != "200" ]
+then
+    echo "Error occurred when installing Gogs Service. HTTP Response $HTTP_RESPONSE"
+    exit 1
+fi
+
+echo
+echo "Initialized Gogs Server...."
+echo
+
+sleep 10
+
+echo
+echo "Setting up wildfly-swarm-s2i repository..."
+echo
+oc rsync -n $OSE_CI_PROJECT $SCRIPT_BASE_DIR/wildfly-swarm-s2i $GOGS_POD:/tmp/
+oc rsh -n $OSE_CI_PROJECT -t $GOGS_POD bash -c "cd /tmp/wildfly-swarm-s2i && git init && git config --global user.email 'gogs@redhat.com' && git config --global user.name 'gogs' && git add . &&  git commit -m 'initial commit'"
+curl -H "Content-Type: application/json" -X POST -d '{"clone_addr": "/tmp/wildfly-swarm-s2i","uid": 1,"repo_name": "wildfly-swarm-s2i"}' --user $GOGS_ADMIN_USER:$GOGS_ADMIN_PASSWORD http://$GOGS_ROUTE/api/v1/repos/migrate
+
+
+echo
+echo "Setting up ose-cicd-api repository..."
+echo
+oc rsync -n $OSE_CI_PROJECT $SCRIPT_BASE_DIR/ose-cicd-api $GOGS_POD:/tmp/
+oc rsh -n $OSE_CI_PROJECT -t $GOGS_POD bash -c "cd /tmp/ose-cicd-api && git init && git config --global user.email 'gogs@redhat.com' && git config --global user.name 'gogs' && git add . &&  git commit -m 'initial commit'"
+curl -H "Content-Type: application/json" -X POST -d '{"clone_addr": "/tmp/ose-cicd-api","uid": 1,"repo_name": "ose-cicd-api"}' --user $GOGS_ADMIN_USER:$GOGS_ADMIN_PASSWORD http://$GOGS_ROUTE/api/v1/repos/migrate
+
+
+
 # Process Jenkins Slave Template
 echo
 echo "Processing Jenkins Slave Template..."
@@ -155,8 +256,6 @@ oc start-build -n ${OSE_CI_PROJECT} jenkins --from-dir="${SCRIPT_BASE_DIR}/infra
 wait_for_running_build "jenkins" "${OSE_CI_PROJECT}"
 
 oc build-logs -n ${OSE_CI_PROJECT} -f jenkins-1
-
-
 
 
 oc project ${OSE_SWARM_INFRA_PROJECT}
